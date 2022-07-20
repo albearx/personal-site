7a827432be069e20469b6055e3cfe361631d7b61
Albert Xu <albert.xu.13103@icloud.com>
2022-07-20 17:50:23 -0400

    Testing for environment variables

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,7 @@
 import './Projects.css'
 const Projects = () => {
+  const secretVar = process.env.REACT_APP_SECRETVAR
+  console.log('environment var:', secretVar)
   return (
     <div className="projects-header">
       <h1>Projects will be added here soon</h1>
